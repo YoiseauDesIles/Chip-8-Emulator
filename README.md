4646761948428b0ef165d99da83bba4040bb839c
YoiseauDesIles <johan.labrado@hotmail.fr>
2023-03-26 00:50:51 +0100

    Maj gitignore + ajout configuration SDL pour mingw32

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # emulateur-nul
-Ceci est un émulateur destiné à emuler la Chip 8
+
+Pour faire fonctionner SDL pour le projet (Windows) : 
+
+ - Creer la variable d'environnement CMAKE_PREFIX_PATH et y mettre le chemin d'accès vers la librairie SDL2_VS
+ - En utilisant le compilateur mingw32 :  
+    - donner le chemin d'accès a SDL : set(SDL2_DIR "C:/_Apps/SDL2_all/32/SDL2")
+    - chercher le paquet SDL2 : find_package(SDL2 REQUIRED)
+    - Inclure les en-tetes SDL2 : include_directories(${SDL2_INCLUDE_DIRS})
+    - Après l'ajout des executables, Lier la bibliothèque SDL2 à l'executable : target_link_libraries(Chip8Emulator ${SDL2_LIBRARIES})
+
+ - En utilisant le compilateur MSVC amd 64 :  
+    - chercher le paquet SDL2 : find_package(SDL2 REQUIRED)
+    - Après l'ajout des executables, Lier la bibliothèque SDL2 à l'executable : target_link_libraries(Chip8Emulator SDL2::SDL2 SDL2::SDL2main)
+
+Dans le main, écrire int main(int argc, char **argv) pour que SDL puisse faire le lien avec SDL_main   
+
+Ajouter la DLL correspondante au même niveau que l'éxecutable généré par Cmake
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
+
 #include <iostream>
 #include "CPU/CPU.h"
+#include <SDL.h>
 
 enum enumTest {a = 0, b = 1};
-int main()
-{
 
+int main(int argc, char **argv){
+
+
+             /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        std::cout << "Échec de l'initialisation de la SDL";
+        return -1;
+    }
+
+    {
+        /* Création de la fenêtre */
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN);
+
+        if( pWindow )
+        {
+            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+
+            SDL_DestroyWindow(pWindow);
+        }
+        else
+        {
+            std::cout << "Erreur de création de la fenêtre:";
+        }
+    }
+
+    SDL_Quit();
+    
+   
+       
+    std::cout << "test";
     CPU cpu = CPU();
     Clock clock = Clock();
     for (int i  = 0; i < 10; i++){
@@ -17,5 +53,5 @@ int main()
     // m.printRamRange(0x200, 0x203);
     
     return 0;
-    
 }
+
